42c4bd4c917f7252b37c4c473f39b1bd2ebadc97
Kishan <you@example.com>
2020-11-05 14:12:07 +0530

    Designing of project

---
+++ oldshop/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class OldshopConfig(AppConfig):
+    name = 'oldshop'
---
+++ oldshop/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .import views
+
+
+
+urlpatterns=[
+    path("",views.home,name="home"),
+    path("login",views.login,name="login"),
+    path("register",views.register,name="register"),
+    path("forgot",views.forgot,name="forgot"),
+    path("contact",views.contact,name="contact"),
+    # path("forgot",views.forgot,name="forgot"),
+    # path("forgot",views.forgot,name="forgot"),
+    
+]
